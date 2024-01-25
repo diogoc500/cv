46a2e714176fda52dbce4f5df7747e37a1912008
Diogo Costa <diogo.alex.nc@gmail.com>
2024-01-25 13:06:46 +0000

    Com

--- lib/sections/about_section.dart
+++ lib/sections/about_section.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:cv_website/sections/section.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +10,43 @@ class AboutSection extends StatelessWidget with SectionWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Container(
       key: sectionKey,
-      height: 500,
+      height: screenHeight,
       color: Colors.black,
       child: Center(
-        child: Text("Hi"),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 100.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.person),
+                  SizedBox(width: 5.0),
+                  Text(
+                    "About Me",
+                    style: TextStyle(fontSize: 25.0),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10.0),
+              Text(
+                "I am a Computer Engineering student with a passion "
+                "for technology. When I'm not working on coding projects, you can often find me playing games, "
+                "watching sci-fi movies or reading a good book (probably fiction). "
+                "I'm always looking for a new challenge and new things to learn, "
+                "specially when it comes to programming.",
+                textAlign: TextAlign.justify,
+                style: TextStyle(fontSize: 18.0),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
