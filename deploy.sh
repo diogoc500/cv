63aff032236d53dc3298a0bfcc99e9ba378bb794
Diogo Costa <diogo.alex.nc@gmail.com>
2024-01-27 00:16:28 +0000

    Created deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+desired_current_branch="development"
+current_branch=$(git branch --show-current)
+
+# Check if the current branch is the desired branch
+if [ "$current_branch" -ne "$desired_branch" ]; then
+  echo "You are not on the $desired_branch branch."
+  exit -1
+fi
+
+flutter clean
+git commit -m "Deploy push."
+git push
+
+git checkout deploy
+git merge development
