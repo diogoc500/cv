bd3d593a834b37658251b6c2d1ae16caf5b284ba
Diogo Costa <diogo.alex.nc@gmail.com>
2024-01-27 00:36:01 +0000

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,10 @@ desired_current_branch="development"
 current_branch=$(git branch --show-current)
 
 # Check if the current branch is the desired branch
-if [ "$current_branch" -ne "$desired_branch" ]; then
-  echo "You are not on the $desired_branch branch."
+if [ "$current_branch" == "$desired_current_branch" ]; then
+  echo "You are on the $desired_current_branch branch."
+else
+  echo "You are not on the $desired_current_branch branch."
   exit -1
 fi
 
